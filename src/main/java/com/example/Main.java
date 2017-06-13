4ec0ea0d15ce739ddc4b0563d2d7b2496e749c7a
Michał Bluj <m.bluj@polsource.com>
2017-06-13 17:29:36 +0200

    rest

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,7 +8,8 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 public class Main {
   public static void main(String[] args) throws IOException {
 
-    final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
+    //final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
+    final String baseUri = "https://jcaesars.herokuapp.com/";
     final Map<String, String> initParams = new HashMap<String, String>();
 
     initParams.put("com.sun.jersey.config.property.packages","resources");
