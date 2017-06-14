b84053c855d56ed55fec0d7c4d8818eea0b4c17a
Michał Bluj <m.bluj@polsource.com>
2017-06-14 08:44:41 +0200

    rest services

--- src/main/java/resources/Rest.java
+++ src/main/java/resources/Rest.java
@@ -1,6 +1,4 @@
-
 package resources;
-
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,13 +6,21 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
 
-@Path("/hello")
-public class Call {
+@Path("/rest")
+public class Rest {
 
     @GET
     @Produces("text/plain")
+    @Path("/helloWorld")
     public String handleGreeting() {
         return "Hello World";
+    }
+
+    @GET
+    @Produces("text/plain")
+    @Path("/time")
+    public String handleTime() {
+        return String.valueOf(System.currentTimeMillis());
     }
 
 }
